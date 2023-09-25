938a917ebd9e2b87d034248f974db9aaedac8fb2
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 11:10:15 -0400

    remove comments

--- src/app/auth/sign-in/route.ts
+++ src/app/auth/sign-in/route.ts
@@ -4,6 +4,9 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+
+
+
 export async function POST(request: Request) {
   const requestUrl = new URL(request.url);
   const formData = await request.formData();
@@ -20,14 +23,12 @@ export async function POST(request: Request) {
     return NextResponse.redirect(
       `${requestUrl.origin}/login?error=Could not authenticate user`,
       {
-        // a 301 status is required to redirect from a POST to a GET route
         status: 301,
       }
     );
   }
 
   return NextResponse.redirect(`${requestUrl.origin}/test`, {
-    // a 301 status is required to redirect from a POST to a GET route
     status: 301,
   });
 }
