ef3fde631c92b3b088e18de6eaf28206473e9acb
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 11:38:54 -0400

    add Image from next/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import {
