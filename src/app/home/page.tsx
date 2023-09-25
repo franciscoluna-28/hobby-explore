e120a504a4dd721983b3a0fdbd4edc5ebef05f1e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 16:35:23 -0400

    add padding and flex containers

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,22 +10,24 @@ export default async function Test() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  console.log(user);
+  console.log( await supabase.auth.getUser());
 
   return (
-    <div>
+    <div className="p-8">
       <h1>Hi! {user?.email}</h1>
       <form action="/auth/sign-out" method="post">
         <button className="button block" type="submit">
           Sign out
         </button>
+
+        <div className="flex gap-4">
         <Link href="/create-activity">
         <Button>Create Activity</Button>
         </Link>
         <Link href="/home/saved-activities">
         <Button>Saved Activities</Button>
         </Link>
-      </form>
+        </div>      </form>
     </div>
   );
 }
