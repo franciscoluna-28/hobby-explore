9bd895654816e992f02402065c03bafd9880fa98
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 11:10:58 -0400

    remove old supabase configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    supabase: {
-        client: {
-            auth: {
-                persistSession: true
-            }
-        }
-      }
+
 }
 
 
