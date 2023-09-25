5b292a255d9110af2e33a3f354081954c4113377
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 12:27:30 -0400

    add domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 const nextConfig = {
-    images: {
-        domains: ["upload.wikimedia.org", "hobby-explore.vercel"]
-    }
-  };
+  images: {
+    domains: ["upload.wikimedia.org", "hobby-explore.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "hobby-explore.vercel.app",
+      },
+    ],
+  },
+};
