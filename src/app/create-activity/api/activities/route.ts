d0b7b17268e0a3ef3d75a175edb13c3cdd5cedf3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 11:08:50 -0400

    isolate helper functions and add suport for tips

--- src/app/create-activity/api/activities/route.ts
+++ src/app/create-activity/api/activities/route.ts
@@ -1,108 +1,82 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { Tips } from "@/tips/tips";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+import {
+  extractActivityData,
+  extractTips,
+  uploadTipsToSupabase,
+} from "@/utils/upload-activity/helpers";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { uploadActivityToSupabase } from "@/utils/upload-activity/upload-activity-to-supabase";
+
+// Supabase client
+const supabase = createRouteHandlerClient({ cookies });
 
 export async function POST(request: Request) {
-  try {
-    const data = await request.formData();
+  const { session } = (await supabase.auth.getSession()).data;
 
-    if (!data) {
+  try {
+    const formData = await request.formData();
+    const activityData = extractActivityData(formData);
+    const tips = extractTips(formData);
+
+    // Check if there are any tips, as an activity must have at least one
+    if (tips.length === 0) {
       return NextResponse.json({
         success: false,
-        error: "No form data found",
+        error: "Activity must have at least one tip!",
         status: 400,
       });
     }
 
-    const formDataValue = data.get("formData");
+    // Check if any tip has an empty text
+    const emptyTipIndices: number[] = tips
+      .map((tip, index) => (tip.text.trim() === "" ? index : -1))
+      .filter((index) => index !== -1);
 
-    if (formDataValue !== null) {
-      const { activityData, tipData } = JSON.parse(formDataValue.toString());
-
-      console.log(activityData);
-
-      const supabase = createRouteHandlerClient({ cookies });
-
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-
-      // Check if a user is authenticated
-      if (!session) {
-        return NextResponse.json({
-          success: false,
-          error: "Not authorized",
-          status: 401,
-        });
-      }
-
-      const user_id = session.user.id;
-
-      // Upload activity to Supabase
-      const { data: activityResult, error: activityError } = await supabase
-        .from("activities")
-        .upsert([
-          {
-            ...activityData,
-            user_id: session.user.id,
-          },
-        ])
-        .select("id");
-
-      if (activityError) {
-        return NextResponse.json({
-          success: false,
-          error: activityError.message,
-          status: 500,
-        });
-      }
-
-      const activity_id = activityResult[0].id;
-
-      // Upload tips and associate them with the activity
-      const tipsWithImageURLs = await Promise.all(
-        tipData.map(async (tip: any) => {
-          const imageFileName = `${uuidv4()}_${tip.name}`;
-          const { data: imageUploadResult, error: imageUploadError } =
-            await supabase.storage
-              .from("tips")
-              .upload(`tips/${user_id}/${imageFileName}`, tip, {
-                contentType: tip.type,
-              });
-
-          if (imageUploadError) {
-            throw new Error(imageUploadError.message);
-          }
-
-          return {
-            activity_id: activity_id,
-            text: tip.text,
-            image_url: imageUploadResult.path,
-          };
-        })
-      );
-
-      // Upload tip data to Supabase
-      const { error: tipError } = await supabase
-        .from("tips")
-        .upsert(tipsWithImageURLs);
-
-      if (tipError) {
-        return NextResponse.json({
-          success: false,
-          error: tipError.message,
-          status: 500,
-        });
-      }
-
-      return NextResponse.json({ success: true, activity_id, status: 200 });
+    if (emptyTipIndices.length > 0) {
+      return NextResponse.json({
+        success: false,
+        error: "Tips must not have empty text!",
+        emptyTipIndices: emptyTipIndices,
+        status: 400,
+      });
     }
-  } catch (error: any) {
+
+    // Upload the activity data to Supabase
+    const activityUploadResult = await uploadActivityToSupabase(
+      supabase,
+      activityData,
+      session?.user.id!
+    );
+
+    if (!activityUploadResult?.success) {
+      throw new Error(activityUploadResult?.error);
+    }
+
+    // Upload the tips data to Supabase, including image URLs
+    const tipsWithImageURLs = await uploadTipsToSupabase(
+      tips as Tips[],
+      session!.user.id,
+      activityUploadResult!.activity_id,
+      supabase
+    );
+
+    // Now with the tips processed with the image URL, upload them
+    await supabase.from("tips").upsert(tipsWithImageURLs);
+
     return NextResponse.json({
-      success: false,
-      error: error.message,
-      status: 500,
+      success: true,
+      activity_id: activityUploadResult!.activity_id,
+      status: 200,
     });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({
+        success: false,
+        error: error.message,
+        status: 500,
+      });
+    }
   }
 }
