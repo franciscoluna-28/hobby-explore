feda5c5c7d2230652768a84adf270ac732c15825
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-25 11:07:01 -0400

    add delete functionality to tips and add textarea

--- src/components/create-tips/tip-list.tsx
+++ src/components/create-tips/tip-list.tsx
@@ -1,23 +1,21 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import useImageStore from "@/store/tips-store";
-import { TiDelete } from "react-icons/ti"
 import { AiFillDelete } from "react-icons/ai";
+import { Textarea } from "../ui/textarea";
 
 export function TipList() {
   const tips = useImageStore((state) => state.tips);
-  const removeTip = useImageStore((state) => state.removeTip)
+  const updateTipText = useImageStore((state) => state.updateText);
+  const removeTip = useImageStore((state) => state.removeTip);
 
-  useEffect(() => console.log(tips), [tips])
-
-  const handleSaveEdit = (tipId: string) => {
-    // Implementa la lógica para guardar la edición del texto utilizando el estado global aquí
+  const handleSaveEdit = (tipId: string, newText: string) => {
+    updateTipText(tipId, newText);
   };
 
   const handleRemoveTip = (tipId: string) => {
-    // Implementa la lógica para eliminar el tip utilizando el estado global aquí
     removeTip(tipId);
   };
-
 
   const [imageData, setImageData] = useState<{ [key: string]: string | null }>(
     {}
@@ -30,7 +28,7 @@ export function TipList() {
       if (e.target && e.target.result) {
         setImageData((prevImageData) => ({
           ...prevImageData,
-          [tipId]: e.target.result as string,
+          [tipId]: e.target!.result as string,
         }));
       }
     };
@@ -47,31 +45,30 @@ export function TipList() {
   }, [tips, imageData]);
 
   return (
-    <div className="flex">
+    <div className="flex gap-4 flex-wrap">
       {tips.map((tip) => (
         <div key={tip.id} className="mb-4">
           {imageData[tip.id] && (
-            <div className="w-96 relative">
-            <img
-              src={imageData[tip.id]!}
-              alt={tip.text}
-              className="w-96 rounded-2xl border bg-blue"
-            />
-            <button
-            onClick={() => handleRemoveTip(tip.id)}
-            className="absolute right-4 top-4 p-2 bg-black/40  z-10 rounded-full text-white cursor-pointer"
-          >
-            <AiFillDelete className="text-xl"/>
-          </button>  
-            
-          <div>
-           
-           
-
-          </div>
-          </div>
+            <div className="w-full relative">
+              <img
+                src={imageData[tip.id]!}
+                alt={tip.text}
+                className="w-96 rounded-2xl max-h-64 border bg-blue"
+              />
+              <button
+                onClick={() => handleRemoveTip(tip.id)}
+                className="absolute right-4 top-4 p-2 bg-white z-10 rounded-full text-white cursor-pointer"
+              >
+                <AiFillDelete className="text-xl text-mainBlack" />
+              </button>
+              <Textarea
+                placeholder="Type your message here."
+                className="rounded-2xl mt-2"
+                value={tip.text}
+                onChange={(e) => handleSaveEdit(tip.id, e.target.value)}
+              />
+            </div>
           )}
-        
         </div>
       ))}
     </div>
