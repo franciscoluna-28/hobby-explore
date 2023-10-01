c96826e36d73ea5384d3e88911d9b77e567a0627
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-01 14:13:00 -0400

    create constants to handle activity uploading

---
+++ src/constants/upload-activity/upload-activity.ts
@@ -0,0 +1,6 @@
+export const UPLOAD_ACTIVITY_CONSTANTS = {
+    successMessage: "Activity uploaded Successfully!",
+    emptyTipTextError: "Tips must not have empty text!",
+    activityTipError: "Activity must have at least one tip!",
+  };
+  
