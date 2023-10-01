4d5565425e31d751e45d5f6c61750ee2545e4900
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-01 14:14:10 -0400

    remove comments

--- src/app/auth/sign-up/route.ts
+++ src/app/auth/sign-up/route.ts
@@ -25,7 +25,6 @@ export async function POST(request: Request) {
     return NextResponse.redirect(
       `${requestUrl.origin}/login?error=Could not authenticate user`,
       {
-        // a 301 status is required to redirect from a POST to a GET route
         status: 301,
       }
     );
@@ -34,7 +33,6 @@ export async function POST(request: Request) {
   return NextResponse.redirect(
     `${requestUrl.origin}/login?message=Check email to continue sign in process`,
     {
-      // a 301 status is required to redirect from a POST to a GET route
       status: 301,
     }
   );
