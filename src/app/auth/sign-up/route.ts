7b6d638e61ed998d85a47c896192b0e3bdead3a5
Francisco Luna <franciscoluna2004@gmail.com>
2023-10-01 22:10:11 -0500

    remove console.log

--- src/app/auth/sign-up/route.ts
+++ src/app/auth/sign-up/route.ts
@@ -19,8 +19,6 @@ export async function POST(request: Request) {
     },
   });
 
-  console.log(error);
-
   if (error) {
     return NextResponse.redirect(
       `${requestUrl.origin}/login?error=Could not authenticate user`,
