cae15f52c0acb77b9de97a264656fff9691491a9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-15 13:42:07 -0400

    create placeholders for data

---
+++ src/database/image-placeholder.ts
@@ -0,0 +1,31 @@
+export type LandingPageImage = {
+  src: string;
+  alt: string;
+};
+
+export const LANDING_PAGE_IMAGES: LandingPageImage[] = [
+  {
+    src: "/images/chess.jpg",
+    alt: "Chess Image",
+  },
+  {
+    src: "/images/dogs.jpg",
+    alt: "Dogs Image",
+  },
+  {
+    src: "/images/friends.jpg",
+    alt: "Friends Image",
+  },
+  {
+    src: "/images/books.jpg",
+    alt: "Books Image",
+  },
+  {
+    src: "/images/coffee.jpg",
+    alt: "Coffee Image",
+  },
+  {
+    src: "/images/piano.jpg",
+    alt: "Piano Image",
+  },
+];
---
+++ src/database/tips-placeholder.ts
@@ -0,0 +1,14 @@
+// constants.js
+export const TIPS = [
+    "Challenge stronger friends in chess to improve your skills. Learn faster!",
+    "Master the basics of a language for rich cultural experiences while traveling.",
+    "Turn hobbies into memorable hangouts with friends. Make lasting memories!",
+    "Travel to new places, embrace the vibes, and collect epic stories.",
+    "Conquer video game challenges for a wild adventure and power-ups.",
+    "Dive into captivating books for fantastic escapes to new worlds.",
+    "Discover thrilling tales in the vibrant world of anime. Begin the journey!",
+    "Stay socially connected and create a treasure trove of memories with friends.",
+    "Balance college life for academic and fun success. It's the college way!",
+    "Compose magical melodies and let your inner musician shine. Feel the music flow!"
+  ];
+  
