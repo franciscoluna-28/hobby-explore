2599d23dea9e080ae41c1662adaa03e943a9a90f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-17 13:41:50 -0400

    add home

--- src/schemas/aboutUserSchema.ts
+++ src/schemas/aboutUserSchema.ts
@@ -1,38 +1,16 @@
 import { z } from "zod";
 
-const MAX_FILE_SIZE = 500000;
-const ACCEPTED_IMAGE_TYPES = [
-  "image/jpeg",
-  "image/jpg",
-  "image/png",
-  "image/webp",
-];
+// Contract for the schema
+interface AboutUserSchema {
+  name: string;
+  birthDate: Date;
+}
 
+// Handles the basic user information
 export const AboutUserSchema = z.object({
   name: z
     .string()
     .min(1, { message: "Your name must have at least 1 character!" }),
   birthDate: z.date(),
-  profilePicture: z
-    .any()
-    .refine((files) => files?.length == 1, "Image is required.")
-    .refine(
-      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-      `Max file size is 5MB.`
-    )
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      ".jpg, .jpeg, .png and .webp files are accepted."
-    ),
-  bannerImage: z
-    .any()
-    .refine((files) => files?.length == 1, "Image is required.")
-    .refine(
-      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-      `Max file size is 5MB.`
-    )
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      ".jpg, .jpeg, .png and .webp files are accepted."
-    ),
+  home: z.string().min(2, {message: "Your home must have at least 2 characters!"})
 });
