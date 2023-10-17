907dd876088a46f38a4bffbee6755f0c6b996955
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-17 13:42:06 -0400

    create isolated user picture schema

---
+++ src/schemas/aboutUserPictures.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+// Constants to deal with image submissions
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+// Contract for the schema
+interface AboutUserPicturesSchema {
+  profilePicture: File;
+  bannerImage: File;
+}
+
+// Handles the file upload requests to handle the user profile
+// Made for customization purposes
+export const AboutUserPicturesSchema = z.object({
+  profilePicture: z
+    .any()
+    .refine((files) => files?.length == 1, "Image is required.")
+    .refine(
+      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+      `Max file size is 5MB.`
+    )
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      ".jpg, .jpeg, .png and .webp files are accepted."
+    ),
+  bannerImage: z
+    .any()
+    .refine((files) => files?.length == 1, "Image is required.")
+    .refine(
+      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+      `Max file size is 5MB.`
+    )
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      ".jpg, .jpeg, .png and .webp files are accepted."
+    ),
+});
