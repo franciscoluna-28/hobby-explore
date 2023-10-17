959f2aec8068aaa71d622d7cef0c14ecb08a0710
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-17 13:44:19 -0400

    create basic layout

---
+++ src/app/get-started/layout.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function GetStartedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="p-16 h-screen items-center flex flex-col justify-center">
+      <div className="p-6 bg-white border shadow-md rounded-2xl">
+        {children}
+      </div>
+    </main>
+  );
+}
+
+{/*       <Image
+        width={150}
+        height={50}
+        alt="Hobby Explore Logo"
+        className="mb-4"
+        src="/logo-web-black.svg"
+      ></Image> */}
