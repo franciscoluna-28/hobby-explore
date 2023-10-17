e5cff68c75e39724056a6947eba26750986528c4
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-10-17 13:41:35 -0400

    create state management in store

--- src/store/about-user-store.ts
+++ src/store/about-user-store.ts
@@ -1,53 +1,38 @@
 import { create } from "zustand";
 
 type State = {
-    profilePicture: File | null,
-    bannerImage: File | null,
-    name: string,
-    birthDate: Date
-}
+  profilePicture: File | null;
+  bannerImage: File | null;
+  name: string;
+  home: string;
+  birthDate: Date;
+};
 
 type Actions = {
-    addProfilePicture: (profilePicture: File) => void;
-    addBannerImage: (bannerImage: File) => void;
-    handleNameChange: (name: string) => void;
-    handleBirthDateChange: (birthDate: Date) => void;   
-}
-
-const initialState: State = {
-    profilePicture: null,
-    bannerImage: null,
-    name: "",
-    birthDate: new Date()
-}
-
+  addProfilePicture: (profilePicture: File) => void;
+  addBannerImage: (bannerImage: File) => void;
+  handleNameChange: (name: string) => void;
+  handleHomeChange: (home: string) => void;
+  handleBirthDateChange: (birthDate: Date) => void;
+};
 
 const useAboutUserStore = create<State & Actions>((set) => ({
-    ...initialState,
-    addProfilePicture: (profilePicture: File) => {
-        set((state) => ({
-            ...state,
-            profilePicture: profilePicture
-        }));
-    },
-    addBannerImage: (bannerImage: File) => {
-        set((state) => ({
-            ...state,
-            bannerImage: bannerImage
-        }));
-    },
-    handleNameChange: (name: string) => {
-        set((state) => ({
-            ...state,
-            name: name
-        }));
-    },
-    handleBirthDateChange: (birthDate: Date) => {
-        set((state) => ({
-            ...state,
-            birthDate: birthDate
-        }));
-    }
+  profilePicture: null,
+  bannerImage: null,
+  name: "",
+  home: "",
+  birthDate: new Date(),
+  addProfilePicture: (profilePicture) => {
+    set((state) => ({ ...state, profilePicture }));
+  },
+  addBannerImage: (bannerImage) => {
+    set((state) => ({ ...state, bannerImage }));
+  },
+  handleNameChange: (value) => set({ name: value }),
+  handleHomeChange: (value) => set({ home: value }),
+  handleBirthDateChange: (birthDate) => {
+    set((state) => ({ ...state, birthDate }));
+  },
 }));
 
 export default useAboutUserStore;
