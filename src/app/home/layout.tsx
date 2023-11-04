98e444a9e31bbed91dc33c2054dba3d2b9a0e965
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-04 15:21:23 -0400

    create final app layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,36 +1,56 @@
+"use client";
+
+import { LinkElement } from "@/components/layout/LinkElement";
 import Image from "next/image";
-import Link from "next/link";
 import { BsCompassFill } from "react-icons/bs";
+import { IoMdNotifications } from "react-icons/io";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { BsBookmarkFill } from "react-icons/bs";
+import { usePathname } from "next/navigation";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function HomeLayout({ children }: Props) {
-  return (
-    <main>
-      <header className="bg-mainGreen flex h-16 items-center m-auto px-4 ">
-        <Image
-          alt="Hobby Explore logo"
-          className=""
-          width={200}
-          height={100}
-          src="/logo-web-white.svg"
-        ></Image>
+  // Call the current path
+  const pathname = usePathname();
 
-        <div className="justify-center m-auto h-full">
-          <div className="relative h-full flex items-center">
-            <Link
-              className="text-white text-normal relative font-bold flex gap-2 items-center"
-              href="/"
-            >
-              <BsCompassFill /> Explore
-            </Link>
-            <div className="absolute h-1 w-full bottom-0 bg-white"></div>
-          </div>
+  const isHomePage = pathname === "/home";
+  const isSavedActivities = pathname === "/home/saved-activities";
+
+  return (
+    <>
+      <header className="bg-mainGreen flex h-16 items-center m-auto px-4 shadow-md">
+        <Image
+          src="/logo-web-white.svg"
+          alt="Hobby Explore Logo"
+          width={150}
+          height={150}
+        />
+
+        <div className="justify-center m-auto h-full flex gap-8 ">
+          <LinkElement href="/home" title="Explore" isActive={isHomePage}>
+            <BsCompassFill />
+          </LinkElement>
+          <LinkElement
+            title="Shortlist"
+            href="/home/saved-activities"
+            isActive={isSavedActivities}
+          >
+            <BsBookmarkFill />
+          </LinkElement>
+        </div>
+        <div className="flex justify-end items-center gap-4">
+          <IoMdNotifications className="text-white text-2xl" />
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
         </div>
       </header>
-      {children}
-    </main>
+
+      <main className="p-8 mx-16">{children}</main>
+    </>
   );
 }
