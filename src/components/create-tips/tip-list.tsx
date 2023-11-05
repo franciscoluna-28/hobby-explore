528e0ed6f46459665f61b2b4267ef1dab8c46da1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-05 10:34:26 -0400

    add animation

--- src/components/create-tips/tip-list.tsx
+++ src/components/create-tips/tip-list.tsx
@@ -1,9 +1,9 @@
-"use client";
 import React, { useState, useEffect } from "react";
 import useImageStore from "@/store/tips-store";
 import { AiFillDelete } from "react-icons/ai";
 import { Textarea } from "../ui/textarea";
 import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 
 export function TipList() {
   const tips = useImageStore((state) => state.tips);
@@ -13,8 +13,6 @@ export function TipList() {
   const handleSaveEdit = (tipId: string, newText: string) => {
     updateTipText(tipId, newText);
   };
-
-  console.log(tips)
 
   const handleRemoveTip = (tipId: string) => {
     removeTip(tipId);
@@ -38,7 +36,6 @@ export function TipList() {
 
     reader.readAsDataURL(file);
   };
-
   useEffect(() => {
     tips.forEach((tip) => {
       if (!imageData[tip.id]) {
@@ -47,35 +44,52 @@ export function TipList() {
     });
   }, [tips, imageData]);
 
+  const animationVariants = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1 },
+    exit: { opacity: 0, scale: 0 },
+  };
+
   return (
-    <div className="flex gap-4 flex-wrap">
-      {tips.map((tip) => (
-        <div key={tip.id} className="mb-4">
-          {imageData[tip.id] && (
-            <div className="w-full relative">
-              <Image
-                src={imageData[tip.id]!}
-                alt={tip.text}
-                width={300}
-                height={300}
-                className="w-96 rounded-2xl max-h-64 border bg-blue"
-              />
-              <button
-                onClick={() => handleRemoveTip(tip.id)}
-                className="absolute right-4 top-4 p-2 bg-white z-10 rounded-full text-white cursor-pointer"
-              >
-                <AiFillDelete className="text-xl text-mainBlack" />
-              </button>
-              <Textarea
-                placeholder="Type your message here."
-                className="rounded-2xl mt-2"
-                value={tip.text}
-                onChange={(e) => handleSaveEdit(tip.id, e.target.value)}
-              />
-            </div>
-          )}
-        </div>
-      ))}
+    <div className="grid grid-cols-2 w-full gap-4">
+      <AnimatePresence>
+        {tips.map((tip) => (
+          <motion.div
+            key={tip.id}
+            className="mb-4"
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={animationVariants}
+          >
+            {imageData[tip.id] && (
+              <div className="w-full relative fit">
+                <Image
+                  src={imageData[tip.id]!}
+                  alt={tip.text}
+                  width={500}
+                  height={500}
+                  className="w-auto rounded-2xl object-contain max-h-64 border bg-blue rounded-b-none"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveTip(tip.id)}
+                  className="absolute border border-black/20 right-4 w-min top-4 p-2 bg-white z-10 rounded-full text-white cursor-pointer"
+                >
+                  <AiFillDelete className="text-xl text-mainBlack" />
+                </button>
+                <Textarea
+                  placeholder="Enter a brief description of your activity"
+                  className="rounded-2xl border-t-0 rounded-t-none"
+                  value={tip.text}
+                  autoFocus
+                  onChange={(e) => handleSaveEdit(tip.id, e.target.value)}
+                />
+              </div>
+            )}
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
