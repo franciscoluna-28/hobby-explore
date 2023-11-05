c1e575f0cbb6e5af60790d3b7b8fc02accd97fdf
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-05 10:35:09 -0400

    update main layout and remove old form

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,69 +1,71 @@
-import { Button } from "@/components/ui/button";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
 import { IoIosAdd } from "react-icons/io";
 import { ACTIVITY_CATEGORIES } from "@/database/activities-categories";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { activitySchema } from "@/schemas/activitySchema";
+import z from "zod";
+import React from "react";
+import Link from "next/link";
 
-const supabase = createServerComponentClient({ cookies });
+const supabase = createClientComponentClient();
 
-export default async function Test() {
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+export default function Test() {
+  const activityForm = useForm<z.infer<typeof activitySchema>>({
+    resolver: zodResolver(activitySchema),
+    defaultValues: {
+      location: "Anywhere",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof activitySchema>) => {
+    console.log(data);
+  };
 
   return (
-    <div className="">
-      <ul className="flex gap-4 rounded-xl">
-        {ACTIVITY_CATEGORIES.map((category) => {
-          return (
-            <li
-              className="bg-gray text-slate-500 p-2 rounded-full px-4"
-              key={category.category_id}
-            >
-              {category.name}
-            </li>
-          );
-        })}
-      </ul>
+    <>
+      <div className="">
+        <ul className="flex gap-4 rounded-xl">
+          {ACTIVITY_CATEGORIES.map((category) => {
+            return (
+              <li
+                className="bg-gray text-slate-500 p-2 rounded-full px-4"
+                key={category.category_id}
+              >
+                {category.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Link href="/create-activity">
+        <div className="cursor-pointer mt-8">
+          <div className="flex justify-between items-center gap-4 bg-white border border-slate-500/10 shadow-sm hover:shadow-md duration-200 rounded-2xl p-4">
+            <div className="flex items-center gap-4">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <span className="text-slate-500 text-sm ">
+                Hey Francisco! Get started by creating some activities and
+                teaching others your favorite hobbies
+              </span>
+            </div>
 
-      <div className="cursor-pointer mt-8">
-        <div className="flex justify-between items-center gap-4 bg-white border border-slate-500/10 shadow-sm hover:shadow-md duration-200 rounded-2xl p-4">
-          <div className="flex items-center gap-4">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <span className="text-slate-500 text-sm ">
-              Hey Francisco! Get started by creating some activities and
-              teaching others your favorite hobbies
-            </span>
-          </div>
-
-          <div className="flex">
-            <div className="bg-mainGreen rounded-full">
-              <IoIosAdd className="text-white text-3xl"></IoIosAdd>
+            <div className="flex">
+              <div className="bg-mainGreen rounded-full">
+                <IoIosAdd className="text-white text-3xl"></IoIosAdd>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/*       <form action="/auth/sign-out" method="post">
-        <button className="button block" type="submit">
-            Sign out
-        </button>
-
-        <div className="flex gap-4">
-          <Link href="/create-activity">
-            <Button>Create Activity</Button>
-          </Link>
-          <Link href="/home/saved-activities">
-            <Button>Saved Activities</Button>
-          </Link>
-        </div>
-      </form>
-
-      <h1>Hi! {user?.email} </h1> */}
-    </div>
+      </Link>
+    </>
   );
 }
