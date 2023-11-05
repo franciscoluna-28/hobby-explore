39b62d1804e0ccd2ac3f5916375aecef16ac0e12
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-05 10:33:20 -0400

    change data structure

--- src/constants/activity-types.ts
+++ src/constants/activity-types.ts
@@ -1,22 +1,24 @@
 export type ActivityType =
-  | "education"
-  | "recreational"
-  | "social"
-  | "diy"
-  | "charity"
-  | "cooking"
-  | "relaxation"
-  | "music"
-  | "busywork";
+  | "Gaming"
+  | "Education"
+  | "Chess"
+  | "Social"
+  | "Cooking"
+  | "Music"
+  | "Busywork"
+  | "Coding"
+  | "Recreational";
 
-export const AVAILABLE_ACTIVITY_TYPES: Record<ActivityType, string> = {
-  education: "Education",
-  recreational: "Recreational",
-  social: "Social",
-  diy: "DIY",
-  charity: "Charity",
-  cooking: "Cooking",
-  relaxation: "Relaxation",
-  music: "Music",
-  busywork: "Busywork",
-};
+  export const AVAILABLE_ACTIVITY_TYPES: { id: number; name: string }[] = [
+    { id: 1, name: "Gaming" },
+    { id: 2, name: "Education" },
+    { id: 3, name: "Chess" },
+    { id: 4, name: "Social" },
+    { id: 5, name: "Cooking" },
+    { id: 6, name: "Music" },
+    { id: 7, name: "Busywork" },
+    { id: 8, name: "Coding" },
+    { id: 9, name: "Recreational" },
+  ];
+  
+  
