aa9414a7f206729e3f38a3820e1a9d951a586031
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-06 16:03:28 -0400

    add types and comments

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,23 +1,22 @@
-import type { NextRequest } from 'next/server'
-import { cookies } from 'next/headers'
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import { Database } from '@/types/supabase'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
+import type { NextRequest } from "next/server";
+import { Database } from "@/lib/types";
 
 export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient<Database>({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient<Database>({
+      cookies: () => cookieStore,
+    });
+    await supabase.auth.exchangeCodeForSession(code);
   }
 
-  if (!code) {
-    return NextResponse.redirect(`${requestUrl.origin}?error=auth_failed`)
-  }
-  
-  return NextResponse.redirect(requestUrl.origin)
+  // URL to redirect to after the sign in process completes
+  return NextResponse.redirect(requestUrl.origin);
 }
