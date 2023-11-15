cff9cc034612f136cb5fe2f82687e5cbb893de51
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-14 21:08:15 -0400

    update types

--- src/constants/activity-types.ts
+++ src/constants/activity-types.ts
@@ -10,17 +10,15 @@ export type ActivityType =
   | "Recreational"
   | "3D - Modelling";
 
-  export const AVAILABLE_ACTIVITY_TYPES: { id: number; name: string }[] = [
-    { id: 1, name: "Gaming" },
-    { id: 2, name: "Education" },
-    { id: 3, name: "Chess" },
-    { id: 4, name: "Social" },
-    { id: 5, name: "Cooking" },
-    { id: 6, name: "Music" },
-    { id: 7, name: "Busywork" },
-    { id: 8, name: "Coding" },
-    { id: 9, name: "Recreational" },
-    {id: 10, name: "3D - Modelling"}
-  ];
-  
-  
+export const AVAILABLE_ACTIVITY_TYPES: { id: number; name: string }[] = [
+  { id: 1, name: "Gaming" },
+  { id: 2, name: "Education" },
+  { id: 3, name: "Chess" },
+  { id: 4, name: "Social" },
+  { id: 5, name: "Cooking" },
+  { id: 6, name: "Music" },
+  { id: 7, name: "Busywork" },
+  { id: 8, name: "Coding" },
+  { id: 9, name: "Recreational" },
+  { id: 10, name: "3D - Modelling" },
+];
