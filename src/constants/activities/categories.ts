8925a1a5d220b49557254984eb42b8bdc9265905
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-01-17 15:22:39 -0400

    feat: create constants

---
+++ src/constants/activities/categories.ts
@@ -0,0 +1,25 @@
+export type ActivitiesCategories = {
+  "Gaming": number,
+  "Education": number,
+  "Chess": number,
+  "Social": number,
+  "Cooking": number,
+  "Music": number,
+  "Busywork": number,
+  "Coding": number,
+  "Recreational": number,
+  "3D - Modelling": number
+};
+
+export const ACTIVITIES_CATEGORIES: ActivitiesCategories = {
+  "Gaming": 1,
+  "Education": 2,
+  "Chess": 3,
+  "Social": 4,
+  "Cooking": 5,
+  "Music": 6,
+  "Busywork": 7,
+  "Coding": 8,
+  "Recreational": 9,
+  "3D - Modelling": 10
+};
---
+++ src/constants/supabase.ts
@@ -0,0 +1,5 @@
+// Root project API endpoint
+const SUPABASE_PROJECT_URL = "https://pnfnkpwwfjrotsossegq.supabase.co";
+
+// Format the root project API endpoint to use the storage
+export const SUPABASE_STORAGE_URL = `${SUPABASE_PROJECT_URL}/storage/v1/object/public/`;
