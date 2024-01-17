89e0cb4b53fe99f1c209b0c2602b37146d39a23f
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-01-17 15:47:33 -0400

    fix: remove old array

--- src/services/activities/categories.ts
+++ src/services/activities/categories.ts
@@ -1,7 +1,8 @@
-import { ACTIVITIES_CATEGORIES } from "@/constants/activities/categories";
+/* import { ACTIVITIES_CATEGORIES } from "@/constants/activities/categories";
 
 export function getCategoryNameById(id: number): string | undefined {
   const category = ACTIVITIES_CATEGORIES.find((category) => category.id === id);
   return category ? category.name : undefined;
 }
 
+ */
