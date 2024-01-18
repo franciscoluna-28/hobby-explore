13275486e330104d07a51dcb8e24bfb8cd55ab99
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-01-17 21:31:41 -0400

    feat: add new update components

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,13 +1,32 @@
+import { LogoutModal } from "@/components/profile/LogoutModal";
+import { ChangeUserNameModal } from "@/components/profile/ChangeUsernameModal";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { getCurrentUser } from "@/services/auth";
 import Link from "next/link";
+import { ChangeDisplayNameModal } from "@/components/profile/ChangeDisplayNameModal";
 
-export default function UserProfileSettings() {
-    return (
-        <div>
-            <h2>User Profile Settings</h2>
-            <Button asChild>
-      <Link href="/profile">Go Back</Link>
-    </Button>
-        </div>
-    )
+export default async function UserProfileSettings() {
+  const user = await getCurrentUser();
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">User Profile Settings</h2>
+      <Separator className="mt-4" />
+      <div className="flex w-min flex-col gap-4 mt-4">
+        <Button asChild className="w-min rounded-[36px]">
+          <Link href="/profile">Go Back</Link>
+        </Button>
+        <ChangeUserNameModal
+          userId={user?.user_id ?? ""}
+          defaultUserUserName={user?.username ?? ""}
+        />
+        <ChangeDisplayNameModal
+          userId={user?.user_id ?? ""}
+          defaultDisplayName={user?.displayName ?? ""}
+        />
+        <LogoutModal />
+      </div>
+    </div>
+  );
 }
