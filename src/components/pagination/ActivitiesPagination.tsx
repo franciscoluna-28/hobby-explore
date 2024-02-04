a9bea834038f052a13f83af394e558a36b94610a
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-02-04 10:41:53 -0400

    fix: update constants

--- src/components/pagination/ActivitiesPagination.tsx
+++ src/components/pagination/ActivitiesPagination.tsx
@@ -9,6 +9,10 @@ import {
 } from "@/components/ui/pagination";
 import { ExistingActivityCategories } from "@/constants/activities/categories";
 import { getExactActivitiesCount } from "@/services/activities/getActivities";
+import {
+  GLOBAL_ACTIVITIES_PER_PAGE,
+  GLOBAL_FIRST_PAGINATION_PAGE,
+} from "@/constants/pagination/globals";
 
 // TODO: MAKE SURE YOU'RE CONTROLLING THE USER URL IF THEY PUT RANDOM STUFF.
 export async function ActivitiesPagination({
@@ -16,21 +20,20 @@ export async function ActivitiesPagination({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const currentPage = Number(searchParams.page) || 1;
+  const currentPage = Number(searchParams.page) || GLOBAL_FIRST_PAGINATION_PAGE;
   const pageCount = await getExactActivitiesCount(
     searchParams.category as undefined | ExistingActivityCategories
   );
-  const ACTIVITIES_PER_PAGE: number = 9;
 
   const getTotalPages = (): number => {
-    const totalPages = Math.ceil((pageCount ?? 0) / ACTIVITIES_PER_PAGE);
+    const totalPages = Math.ceil((pageCount ?? 0) / GLOBAL_ACTIVITIES_PER_PAGE);
     return Math.max(1, totalPages);
   };
 
   const totalPages = getTotalPages();
 
   const isDisabled = (targetPage: number): boolean =>
-    targetPage <= 1 || targetPage >= totalPages;
+    targetPage <= GLOBAL_FIRST_PAGINATION_PAGE || targetPage >= totalPages;
 
   const renderPageNumbers = (): JSX.Element[] => {
     return Array.from({ length: totalPages }, (_, index) => index + 1).map(
@@ -38,7 +41,8 @@ export async function ActivitiesPagination({
         <PaginationItem key={pageNumber}>
           <PaginationLink
             isActive={
-              currentPage === pageNumber || (pageNumber === 1 && !currentPage)
+              currentPage === pageNumber ||
+              (pageNumber === GLOBAL_FIRST_PAGINATION_PAGE && !currentPage)
             }
             href={generateLink(pageNumber)}
           >
@@ -59,10 +63,14 @@ export async function ActivitiesPagination({
         <PaginationItem>
           <PaginationPrevious
             className={
-              currentPage <= 1 ? "pointer-events-none opacity-50" : undefined
+              currentPage <= GLOBAL_FIRST_PAGINATION_PAGE
+                ? "pointer-events-none opacity-50"
+                : undefined
             }
-            aria-disabled={currentPage <= 1}
-            tabIndex={currentPage <= 1 ? -1 : undefined}
+            aria-disabled={currentPage <= GLOBAL_FIRST_PAGINATION_PAGE}
+            tabIndex={
+              currentPage <= GLOBAL_FIRST_PAGINATION_PAGE ? -1 : undefined
+            }
             href={generateLink(currentPage - 1)}
           />
         </PaginationItem>
