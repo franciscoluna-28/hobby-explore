b343b2025c23964b0440c200095f0779cd5cc341
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-02-11 16:50:59 -0400

    style: update UI design

--- src/components/profile/LogoutModal.tsx
+++ src/components/profile/LogoutModal.tsx
@@ -13,10 +13,19 @@ import {
 import { IoMdExit } from "react-icons/io";
 
 import { Button } from "../ui/button";
+import { Card } from "../ui/card";
+import { LogOut } from "lucide-react";
 
 export function LogoutModal() {
   return (
-
+    <Card className=" flex items-center space-x-4 rounded-md border p-4">
+      <LogOut className="w-6 h-6" />
+      <div className="flex-1 space-y-1">
+        <p className="text-sm font-medium leading-none">Log out</p>
+        <p className="text-sm text-muted-foreground">
+          Close the current section.
+        </p>
+      </div>
       <AlertDialog>
         <AlertDialogTrigger type="button" asChild>
           <Button
@@ -41,18 +50,16 @@ export function LogoutModal() {
           <AlertDialogFooter>
             <AlertDialogCancel>Cancel</AlertDialogCancel>
             <form action="/auth/sign-out" method="post">
-            <Button
-              type="submit"
-              
-              className="bg-red-500 text-white hover:bg-red-600 duration-200"
-            >
-              Log Out
-              
-            </Button>
+              <Button
+                type="submit"
+                className="bg-red-500 text-white hover:bg-red-600 duration-200"
+              >
+                Log Out
+              </Button>
             </form>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-
+    </Card>
   );
 }
