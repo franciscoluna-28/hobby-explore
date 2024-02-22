dd6e19a3e4ed8af2d4578fc1b9fa7d159d0fc0e1
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-02-22 16:05:38 -0400

    build: update dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -9,6 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    setupFiles: ['dotenv/config'],
     environment: 'jsdom',
     globals: true
   },
