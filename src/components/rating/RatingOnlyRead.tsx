f77f6fb2e67918a063d22640672899390d3fff5c
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-02-28 08:35:36 -0400

    style: update UI

--- src/components/rating/RatingOnlyRead.tsx
+++ src/components/rating/RatingOnlyRead.tsx
@@ -2,6 +2,7 @@
 
 import { Tables } from "@/lib/database";
 import { useGetActivityCountRating } from "@/hooks/activities/useGetActivityCountRating";
+import { Star } from "lucide-react"
 
 type RatingOnlyReadProps = {
   activityId: Tables<"activities_rating">["activity_id"];
@@ -10,9 +11,14 @@ type RatingOnlyReadProps = {
 export function RatingReadOnly({ activityId }: RatingOnlyReadProps) {
   const { initialData } = useGetActivityCountRating({ activityId: activityId });
 
+
+
   return (
-    <div>
-      <p>{initialData}</p>
+    <div className="flex gap-2 items-center">
+      <Star fill="#F6B704" strokeWidth={0}/>
+      <p className="text-[#F6B704] text-sm">{initialData?.rating}</p>
+      <p className="text-[#D9D9D9] text-sm">({initialData?.count})</p>
+
     </div>
   );
 }
