dd02957da5702df91030f766d1707c92c7e2dc8d
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-03-10 11:52:21 -0400

    feat: add conditional rendering to handle different activities cases

--- src/components/activities/NoExistingSavedActivities.tsx
+++ src/components/activities/NoExistingSavedActivities.tsx
@@ -4,7 +4,13 @@ import NotFound from "../../../public/not-found.webp";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
-export function NoExistingSavedActivities() {
+type NoExistingSavedActivitiesProps = {
+  shouldBeYourOwnActivities?: boolean;
+};
+
+export function NoExistingSavedActivities({
+  shouldBeYourOwnActivities = false,
+}: NoExistingSavedActivitiesProps) {
   return (
     <ActivityMotion>
       <div className="m-auto flex justify-center flex-col items-center my-12 gap-3">
@@ -18,12 +24,18 @@ export function NoExistingSavedActivities() {
         />
         <h4 className="font-bold text-mainBlack my-3 text-4xl">Oops...</h4>
         <div className="flex flex-col">
-        <span className="text-slate-500 block">
-          It seems you don&apos;t have saved activities yet...
-          
-        </span>
-        <Button variant="ghost" className="my-2" asChild>
-          <Link className="text-mainGreen font-medium" href="/app/explore">Find Some Here</Link>
+          <span className="text-slate-500 block">
+            {shouldBeYourOwnActivities
+              ? "It seems you haven't created an activity yet. Get started by creating a new one."
+              : "It seems you don't have saved activities yet..."}
+          </span>
+          <Button variant="ghost" className="my-2" asChild>
+            <Link
+              className="text-mainGreen font-medium"
+              href={!shouldBeYourOwnActivities ? "/app/explore" : "/app/create"}
+            >
+              {!shouldBeYourOwnActivities ? "Find some here" : "Start by creating a new activity"}
+            </Link>
           </Button>
         </div>
       </div>
