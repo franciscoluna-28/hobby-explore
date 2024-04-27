2e969a4af22536801bd5386c00243375d9f2d0a7
franciscoluna-28 <franciscoluna2004@gmail.com>
2024-04-27 10:34:43 -0400

    fix: add &apos;

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -26,8 +26,8 @@ export function HeroSection() {
         </h1>
         <p className="text-mainBlack/80 text-[18px] mt-4 dark:text-white/60">
           Tired of the same boring hobbies? Welcome to&nbsp;Hobby Explore, your
-          gateway to a vibrant world of hobbies and innovation where you'll be
-          able to share your passions with other people and connect together.
+          gateway to a vibrant world of hobbies and innovation where you&apos;ll be
+          able to share your passions with other people.
         </p>
         <Button className="mt-8" asChild>
           <Link href="/app/explore">Try it for free</Link>
